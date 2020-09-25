5ef44a4011fc7a27b230c8614960d8da48cebfac
DariaBerashevich <71172779+DariaBerashevich@users.noreply.github.com>
2020-09-25 21:41:44 +0300

    fix: repair

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 const k = 0.693/HALF_LIFE_PERIOD;
 module.exports = function dateSample(sampleActivity) {
+    throw new CustomError('Not implemented');
   if (typeof sampleActivity !== 'string') return false;
   if (sampleActivity > MODERN_ACTIVITY) return false;
   if (sampleActivity <= 0) return false;
